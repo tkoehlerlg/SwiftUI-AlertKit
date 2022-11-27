90bb2797d4e287d625ea886387330767ea809f44
Torben Köhler <t.koehler@schueler.lg-ks.de>
2022-11-27 12:12:50 +0100

    fix Environment accessibility

--- Sources/AlertKit/View+Extension.swift
+++ Sources/AlertKit/View+Extension.swift
@@ -8,9 +8,22 @@
 import Foundation
 import SwiftUI
 
+struct GlobalAlertViewModifier: ViewModifier {
+    @Environment(\.alertState) var alertState
+    var alert: Alert?
+
+    func body(content: Content) -> some View {
+        content
+            .alert(alert, alertState: alertState)
+    }
+}
+
 extension View {
     public func alert(_ alert: Alert?) -> some View {
-        @Environment(\.alertState) var alertState
+        self.modifier(GlobalAlertViewModifier(alert: alert))
+    }
+
+    func alert(_ alert: Alert?, alertState: AlertState) -> some View {
         if let alert = alert {
             withAnimation(.easeOut(duration: 0.3).delay(0.5)) {
                 alertState.addAlert(alert)
